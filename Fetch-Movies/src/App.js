77e99186926aa726bc0765fcd84fb29a7d3d4556
Asif Imam <63618041+theasifimam@users.noreply.github.com>
2022-11-19 16:00:58 +0530

    Add files via upload

---
+++ Fetch-Movies/src/App.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect, useCallback } from "react";
+
+import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
+import "./App.css";
+import Loading from "./components/Loading";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      // const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://react-http-6b7a6-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+
+      let loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      // const transformedMovies = data.results.map((movieData) => {
+      // const transformedMovies = loadedMovies.map((movieData) => {
+      //   return {
+      //     id: movieData.key,
+      //     title: movieData.title,
+      //     openingText: movieData.openingText,
+      //     releaseDate: movieData.releaseDate,
+      //   };
+      // });
+      setMovies(loadedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    console.log(movie);
+    const response = await fetch(
+      "https://react-http-6b7a6-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json",
+
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <Loading />;
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ Fetch-Movies/src/components/Loading.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="wrapper">
+      <div className="loader">
+        <div className="spinner"></div>
+      </div>
+      <div className="loader">
+        <div className="spinner"></div>
+      </div>
+      <div className="loader">
+        <div className="spinner"></div>
+      </div>
+      <div className="loader">
+        <div className="spinner"></div>
+      </div>
+      <div className="loader">
+        <div className="spinner"></div>
+      </div>
+      <div className="loader">
+        <div className="spinner"></div>
+      </div>
+      <div className="loader">
+        <div className="spinner"></div>
+      </div>
+      <div className="loader">
+        <div className="spinner"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
